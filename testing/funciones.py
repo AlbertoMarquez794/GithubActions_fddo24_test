d6e90e635aef7be590241495386628df8d06f120
albertomarquez794 <thebrayanxd45@gmail.com>
2024-11-23 23:47:07 -0600

    Bueno

--- testing/funciones.py
+++ testing/funciones.py
@@ -1,7 +1,11 @@
 def es_primo(numero):
     """Determina si un número es primo."""
+    if not isinstance(numero, int):
+        raise ValueError("El argumento debe ser un número entero.")
+
     if numero < 2:
         return False
+
     for i in range(2, int(numero**0.5) + 1):
         if numero % i == 0:
             return False
