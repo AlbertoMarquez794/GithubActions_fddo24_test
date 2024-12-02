d9014411c87584a959045fad0ce1f54cb19e018f
Diego Mendez <yeyomendez2003@hotmail.com>
2024-12-02 23:56:17 +0000

    tarea 208400

---
+++ tareas/208400/funcion.py
@@ -0,0 +1,8 @@
+# funcion.py
+def cuadrado(num):
+    """
+    Devuelve el cuadrado de un número.
+    :param num: Número al que se le calculará el cuadrado.
+    :return: El cuadrado del número.
+    """
+    return num ** 2
